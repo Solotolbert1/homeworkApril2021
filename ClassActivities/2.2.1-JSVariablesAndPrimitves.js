154f315ab70b192dd415d5fb3ea6f5e74a399f9f
Raheem Tolbert <rtolbert32@gmail.com>
2021-04-20 19:47:36 -0500

    JS Variables and Primitives

---
+++ ClassActivities/2.2.1-JSVariablesAndPrimitves.js
@@ -0,0 +1,28 @@
+// create a constant variable (const)
+//const name = "Raheem";
+//console.log(name);
+// create a variable that can be reassigned (let)
+let age = 12;
+console.log(age);
+// create three variables and assign them values of different data types (=)
+let house ="Green";
+let pet =6;
+let happiness = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof house);
+console.log(typeof pet);
+console.log(typeof happiness);
+// change the value referenced by a variable (dynamic typing)
+pet = "Charlie";
+
+// print the type of the variable you just changed (typeof)
+console.log(typeof pet);
+
+// create variables and give them values to complete the sentences that will print
+// ? which variables need to be created?
+// ? what type of data needs to go in each variable?
+const name = "David";
+let numberOfWeeks = "5 weeks";
+
+console.log("Hello my name is and "  +  name  +  " and I have been learning Javascript for " + numberOfWeeks + ".");
