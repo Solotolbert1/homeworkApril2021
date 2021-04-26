d721bc7b8fcb6f70580635912fa2069dc3bdc5e9
Raheem Tolbert <rtolbert32@gmail.com>
2021-04-26 15:07:54 -0500

    Week 2 Pizza Place HW

---
+++ .vscode/Week2Homework-PizzaPlace.js
@@ -0,0 +1,16 @@
+let pizzaPlace ="Place of Pizza"
+let numberOfToppings = 10
+
+ console.log(pizzaPlace)
+ console.log(numberOfToppings)
+ console.log(typeof pizzaPlace)
+
+console.log(typeof numberOfToppings)
+
+ console.log(`The name of the new restaurant is ${pizzaPlace} and they have ${numberOfToppings} toppings`)
+
+ if (numberOfToppings >= 10){
+    console.log("A whole lot of pizza.")
+ } else if (numberOfToppings < 10) {
+   console.log("Quality, not quantity")
+ }
