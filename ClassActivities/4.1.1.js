9cd01e79a5c1ca6d70aab31663f7a61e6feff2de
Raheem Tolbert <rtolbert32@gmail.com>
2021-05-03 19:29:32 -0500

    DOM work

---
+++ ClassActivities/4.1.1.js
@@ -0,0 +1,18 @@
+// select all unordered list (ul) elements
+document.querySelectorAll("ul");
+// select all elements with the class "class-week"
+document.querySelectorAll(".class-week");
+// select all elements with the class "nav-bar"
+document.querySelectorAll(".nav-bar");
+// select the element with the id "dog-picture" and save it to a variable
+let doggoPicture =document.querySelector("#dog-picture);
+
+
+# While you are working, you can see what is printed to the console by typing 'node 4.1.1-SelectingDOMElements.js' in the terminal
+
+# When this activity is finished, push your work up to GitHub
+1. Type `git add 4.1.1-SelectingDOMElements.js` in your terminal
+2. Type `git status`
+3. Type `git commit -m "Selecting DOM Elements"`
+4. Type `git status`
+5. Type `git push`
